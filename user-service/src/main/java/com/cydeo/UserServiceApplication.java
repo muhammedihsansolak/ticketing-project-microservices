49e27be43e2503f64d410a97a3f2459f9df9700b
Muhammed <muhammedihsansolak@gmail.com>
2023-12-20 15:03:59 +0300

    Eureka added

--- user-service/src/main/java/com/cydeo/UserServiceApplication.java
+++ user-service/src/main/java/com/cydeo/UserServiceApplication.java
@@ -3,10 +3,12 @@ package com.cydeo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
