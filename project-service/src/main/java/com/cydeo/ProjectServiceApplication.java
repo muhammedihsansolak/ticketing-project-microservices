a9d06bdc0b465aba5d545d39489fc74134f68b80
Muhammed <muhammedihsansolak@gmail.com>
2023-12-21 15:44:08 +0300

    Gateway added

--- project-service/src/main/java/com/cydeo/ProjectServiceApplication.java
+++ project-service/src/main/java/com/cydeo/ProjectServiceApplication.java
@@ -4,11 +4,13 @@ package com.cydeo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ProjectServiceApplication {
 
     public static void main(String[] args) {
--- task-service/src/main/java/com/cydeo/TaskServiceApplication.java
+++ task-service/src/main/java/com/cydeo/TaskServiceApplication.java
@@ -3,12 +3,14 @@ package com.cydeo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
