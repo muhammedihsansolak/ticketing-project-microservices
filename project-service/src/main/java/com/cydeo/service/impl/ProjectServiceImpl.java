f953331d624cec93ea562d7266cf5b57fd1ec9ea
Muhammed <muhammedihsansolak@gmail.com>
2023-12-22 11:26:44 +0300

    Circuit Breaker Resilience4j added

--- project-service/src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ project-service/src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -12,8 +12,10 @@ import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserClientService;
 import com.cydeo.util.MapperUtil;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -106,6 +108,7 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
+    @CircuitBreaker(name = "user-service", fallbackMethod = "userServiceFallBack")
     public List<ProjectDTO> listAllProjectDetails(String userName) throws ProjectServiceException {
 
         UserResponseDTO userResponseDto = userClientService.getUserDTOByUserName(userName);
@@ -130,6 +133,11 @@ public class ProjectServiceImpl implements ProjectService {
             }).collect(Collectors.toList());
         }
         throw new ProjectServiceException("user couldn't find");
+    }
+
+    private List<ProjectDTO> userServiceFallBack(String userName,Exception e){
+        //some business logic here
+        return new ArrayList<>();
     }
 
 
