f707487ae76ef636f7d70570377f9ca07e952ffa
Muhammed <muhammedihsansolak@gmail.com>
2023-12-26 10:40:55 +0300

    Monitoring tools Prometheus, Grafana, Sleuth, Zipkin added

--- user-service/src/main/java/com/cydeo/UserServiceApplication.java
+++ user-service/src/main/java/com/cydeo/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.cydeo;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.http.ApiVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
