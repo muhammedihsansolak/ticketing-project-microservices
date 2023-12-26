512101246435fdfb6a242b7d89803ef023d32f45
Muhammed <muhammedihsansolak@gmail.com>
2023-12-26 12:54:09 +0300

    Config server implemented

--- user-service/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO roles(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, description)
-VALUES ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Admin'),
-       ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Manager'),
-       ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Employee');
